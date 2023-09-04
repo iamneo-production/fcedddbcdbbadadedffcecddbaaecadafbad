cf8c6020186741daea11c9128aae6fd758f4198b
Iamneo <techadmin@iamneo.ai>
2023-09-04 13:47:49 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-
+import SignIn from './SignIn';
 // Home component
 const Home = () => (
   <div>
