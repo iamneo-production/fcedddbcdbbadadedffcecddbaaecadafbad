9999353ffc675b605510b4c6d831efbe5c7cbc81
Iamneo <techadmin@iamneo.ai>
2023-09-13 09:38:00 +0000

    Backup Commit

--- reactapp/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-axios.get('http://localhost:8081/api/register')
-  .then((response) => {
-    console.log(response.data);
-  })
-  .catch((error) => {
-    console.error(error);
-  });
