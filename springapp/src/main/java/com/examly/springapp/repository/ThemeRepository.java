725daaeb5287756c2099fed6d9d4f0041f38a86d
Iamneo <techadmin@iamneo.ai>
2023-09-13 16:41:36 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/repository/ThemeRepository.java
+++ springapp/src/main/java/com/examly/springapp/repository/ThemeRepository.java
@@ -1,6 +1,6 @@
 package com.examly.springapp.repository;
 
-import com.examly.springapp.Theme;
+import com.examly.springapp.entity.Theme;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
