cc060d4752e7aa4a88829ed515e1b30398622d53
Iamneo <techadmin@iamneo.ai>
2023-09-14 09:17:04 +0000

    Backup Commit

---
+++ springapp/src/main/java/com/examly/springapp/config/SecurityConfiguration.java
@@ -0,0 +1,27 @@
+package com.examly.springapp.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .anyRequest().permitAll()
+                .and()
+                .httpBasic();
+        return http.build();
+    }
+
+}
