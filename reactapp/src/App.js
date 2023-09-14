4447ed77a33797b0f3c8f3f6b7c54f96b256858f
Iamneo <techadmin@iamneo.ai>
2023-09-14 16:16:15 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,43 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Register from './Register'
+import logo from './logo.svg';
+import './App.css';
 
-// Home component
-const Home = () => (
-  <div>
-    <h2>Home Page</h2>
-    <p>Welcome to the Home page!</p>
-  </div>
-);
-// About component
-const About = () => (
-  <div>
-    <h2>About Page</h2>
-    <p>This is the About page.</p>
-  </div>
-);
 function App() {
   return (
-    <Router>
-      <div>
-      <Register />
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path="/about" element={<About />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </div>
-    </Router>
-    
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
+
 export default App;
