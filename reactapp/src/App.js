7a09d5dd8ca99196eeb6ff9be872b10891236df2
Iamneo <techadmin@iamneo.ai>
2023-09-23 11:59:57 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,19 +1,42 @@
+// eslint-disable-next-line
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Homepage from './components/User/Homepage';
+import Myorder from './components/User/Myorder';
+import PlaceOrder from './components/User/PlaceOrder';
+import NoPage from './components/NoPage';
 import Layout from './components/Layout';
-import Login from './components/Login';
-import Registration from './components/Registration';
+import Login from './components/Login'; 
+import Registration from './components/Registration'; 
 import './styles.css';
+import 'react-bootstrap/dist/react-bootstrap';
+import AdminGifts from './components/Admin/AdminGifts';
+import AdminThemes from './components/Admin/AdminThemes';
+import Adminvieworders from './components/Admin/Adminvieworders';
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Login />} />
-          <Route path="Registration" element={<Registration />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+    <Route path="/" element={<Layout />}>
+        <Route index element={<Homepage />} />
+        <Route path="Homepage" element={<Homepage />} />
+        <Route path="/myorder/:giftName/:giftPrice" component={Myorder} />
+        <Route path="Myorder" element={<Myorder />} />
+        <Route path="PlaceOrder" element={<PlaceOrder />} />
+        <Route path="Login" element={<Login />} />
+        <Route path="Logout" element={<Login />} />
+        <Route path="Registration" element={<Registration />} />
+        <Route path="AdminGifts" element={<AdminGifts />} />
+        <Route path="Adminvieworders" element={<Adminvieworders />} />
+        <Route path="AdminThemes" element={<AdminThemes />} /> {/* Add a route for AdminGifts */}
+        <Route path="*" element={<NoPage />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
   );
 }
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
---
+++ reactapp/src/components/User/PlaceOrder.js
@@ -0,0 +1,175 @@
+import React, { Component } from 'react';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import Nav from 'react-bootstrap/Nav';
+import { Link } from 'react-router-dom';
+
+class PlaceOrder extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      orders: [],  
+      editingIndex: -1,  
+      editedGiftName: '',  
+      editedPrice: '', 
+      editedQuantity: '',  
+    };
+  }
+
+  handleInputChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  addOrder = () => {
+    // Add your code to add an order here if needed
+  };
+
+  removeOrder = (index) => {
+    const { orders } = this.state;
+    orders.splice(index, 1);
+    this.setState({ orders });
+  };
+
+  editOrder = (index) => {
+    const { orders } = this.state;
+    const { giftName, price, quantity } = orders[index];
+
+    this.setState({
+      editingIndex: index,
+      editedGiftName: giftName,
+      editedPrice: price.toString(),
+      editedQuantity: quantity.toString(),
+    });
+  };
+
+  saveOrder = (index) => {
+    const { orders, editedGiftName, editedPrice, editedQuantity } = this.state;
+
+    const updatedOrder = { ...orders[index], giftName: editedGiftName, price: parseFloat(editedPrice), quantity: parseInt(editedQuantity, 10) };
+    const updatedOrders = [...orders];
+    updatedOrders[index] = updatedOrder;
+
+    this.setState({
+      orders: updatedOrders,
+      editingIndex: -1,  
+      editedGiftName: '',
+      editedPrice: '',
+      editedQuantity: '',
+    });
+  };
+
+  render() {
+    const { orders, editingIndex, editedGiftName, editedPrice, editedQuantity } = this.state;
+
+    return (
+      <div style={{ backgroundColor: 'grey', minHeight: '100vh' }}>
+        <div style={{ margin: '20px' }}>
+          <Nav defaultActiveKey="/Homepage" as="ul" className="justify-content-center" style={{ backgroundColor: 'green' }}>
+            <div className="d-flex" style={{ fontWeight: 'bold' }}>
+              <div className="nav-item mx-2">
+                <Link to="/Myorder" className="nav-link" style={{ color: 'white', fontSize: '20px' }}>
+                  My order
+                </Link>
+              </div>
+              <div className="nav-item mx-2">
+                <Link to="/Homepage" className="nav-link" style={{ color: 'white', fontSize: '30px' }}>
+                  Home
+                </Link>
+              </div>
+              <div className="nav-item ml-auto">
+                <Link to="/Login" className="nav-link" style={{ color: 'white', fontSize: '20px' }}>
+                  Logout
+                </Link>
+              </div>
+            </div>
+          </Nav>
+        </div>
+        <Container>
+          <Row>
+            <Col md={12}>
+              <h2 style={{ fontSize: '24px' }}></h2>
+              <table className="table">
+                <thead>
+                  <tr style={{ backgroundColor: 'darkgreen' }}>
+                    <th>Gift Name</th>
+                    <th>Price</th>
+                    <th>Quantity</th>
+                    <th>Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {orders.map((order, index) => (
+                    <tr key={index} style={{ backgroundColor: 'lightgreen' }}>
+                      <td>
+                        {editingIndex === index ? (
+                          <input
+                            type="text"
+                            name="editedGiftName"
+                            value={editedGiftName}
+                            onChange={(e) => this.handleInputChange(e)}
+                            style={{ width: '100px' }}
+                          />
+                        ) : (
+                          order.giftName
+                        )}
+                      </td>
+                      <td>
+                        {editingIndex === index ? (
+                          <input
+                            type="text"
+                            name="editedPrice"
+                            value={editedPrice}
+                            onChange={(e) => this.handleInputChange(e)}
+                            style={{ width: '100px' }}
+                          />
+                        ) : (
+                          order.price
+                        )}
+                      </td>
+                      <td>
+                        {editingIndex === index ? (
+                          <input
+                            type="text"
+                            name="editedQuantity"
+                            value={editedQuantity}
+                            onChange={(e) => this.handleInputChange(e)}
+                            style={{ width: '100px' }}
+                          />
+                        ) : (
+                          order.quantity
+                        )}
+                      </td>
+                      <td>
+                        {editingIndex === index ? (
+                          <>
+                            <Button variant="success" onClick={() => this.saveOrder(index)} style={{ marginRight: '10px' }}>
+                              Save
+                            </Button>
+                            <Button variant="secondary" onClick={() => this.setState({ editingIndex: -1 })}>
+                              Cancel
+                            </Button>
+                          </>
+                        ) : (
+                          <>
+                            <Button variant="warning" onClick={() => this.editOrder(index)} style={{ marginRight: '10px' }}>
+                              Edit
+                            </Button>
+                            <Button variant="danger" onClick={() => this.removeOrder(index)}>
+                              Remove
+                            </Button>
+                          </>
+                        )}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default PlaceOrder;
