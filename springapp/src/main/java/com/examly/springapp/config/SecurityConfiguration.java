399f447e009a4e50e70e5ad20572f354da552c47
Iamneo <techadmin@iamneo.ai>
2023-09-23 11:49:58 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/config/SecurityConfiguration.java
+++ springapp/src/main/java/com/examly/springapp/config/SecurityConfiguration.java
@@ -9,11 +9,25 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.Customizer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
-
 
     private final UserServiceImpl userServiceImpl;
 
@@ -23,26 +37,31 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                .csrf().disable()
-//                .authorizeRequests(auth ->
-//                    auth.antMatchers("/admin/signup", "/admin/login",
-//                                    "/user/signup", "/user/login").permitAll()
-//                        .anyRequest().authenticated()
-//                )
-//                .userDetailsService(userServiceImpl)
-//                .httpBasic(Customizer.withDefaults());
-//        return http.build();
         http
                 .csrf().disable()
-                .authorizeHttpRequests()
-//                .antMatchers("/v3/api-docs/**",
-//                        "/swagger-ui/**",
-//                        "/swagger-ui.html").permitAll()
-                .anyRequest().permitAll()
+                //.cors() // Enable CORS
+                //.and()
+                .authorizeRequests()
+                .antMatchers("/admin/signup", "/admin/login", "/user/signup", "/user/login","admin/addGift").permitAll() // These routes are permitted without authentication
+                //.anyRequest().authenticated() // All other requests require authentication
                 .and()
-                .httpBasic();
+                .httpBasic(); // Enable HTTP Basic Authentication
+
         return http.build();
     }
+
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(false);
+        config.setAllowedOriginPatterns(List.of("*"));
+        config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "responseType", "Authorization"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
 
 }
